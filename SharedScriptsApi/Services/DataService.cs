38571c0b02b0fffb67fe3fcac170e05ae0a4e3cb
Jason Oehlberg <JOehlberg@saltustech.com>
2025-12-18 13:48:32 -0600

    add generic service --not sure if needed

---
+++ SharedScriptsApi/Services/DataService.cs
@@ -0,0 +1,9 @@
+﻿using SharedScriptsApi.Interfaces;
+
+namespace SharedScriptsApi.Services
+{
+    public class DataService<T> : IDataService<T>
+        where T : class
+    {
+    }
+}
---
+++ SharedScriptsApi/Services/ScriptService.cs
@@ -0,0 +1,17 @@
+﻿using SharedScriptsApi.Interfaces;
+using SharedScriptsApi.DataModels;
+
+namespace SharedScriptsApi.Services
+{
+    public class ScriptService<T> : DataService<T>
+        where T :Script
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public ScriptService(IServiceProvider serviceProvider) 
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+
+    }
+}
