86b3e15a87203f6188de712d578fbf604ad8e847
Jason Oehlberg <JOehlberg@saltustech.com>
2025-12-22 12:10:14 -0600

    add connection type enum --may not need

---
+++ SharedScriptsApi/Enums/DbConnectionType.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedScriptsApi.Enums
+{
+    //[Flags]
+    //public enum DbConnectionType
+    //{
+    //    [Display(Name = "Customer")]
+    //    Customer = 0x001,
+    //    [Display(Name = "State")]
+    //    State = 0x002,
+    //    [Display(Name = "Core")]
+    //    Core = 0x004,
+    //}
+
+    public enum DbConnectionType
+    {
+        None = 0,
+        [Display(Name = "Customer")]
+        Customer = 1,
+        [Display(Name = "Core")]
+        Core = 2,
+        [Display(Name = "Oklahoma")]
+        Oklahoma = 3,
+    }
+}
