3c104adba0addf964201a4fede75d9defc779f86
Jason Oehlberg <JOehlberg@saltustech.com>
2025-12-23 11:26:22 -0600

    add crytography provider

---
+++ SharedScriptsApi/Interfaces/ICryptographyProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedScriptsApi.Interfaces
+{
+    public interface ICryptographyProvider
+    {
+        string Encrypt(string value);
+        string Encrypt(string value, string password, string salt);
+        string Decrypt(string value);
+        string Decrypt(string value, string password, string salt);
+        byte[] Decrypt(string password, string salt, byte[] buffer, int offset, int count);
+        byte[] Decrypt(byte[] key, byte[] buffer, int offset, int count);
+        byte[] GetKey(string password, string salt);
+    }
+}
--- SharedScriptsApi/Utilities/CryptographyProvider.cs
+++ SharedScriptsApi/Utilities/CryptographyProvider.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Text;
-using Saltus.digiTICKET.DataInterfaces;
+using SharedScriptsApi.Interfaces;
 
 namespace SharedScriptsApi.Utilities
 {
