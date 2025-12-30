566dd6491f622c9429aa99401f52e3b76772ef90
Jason Oehlberg <JOehlberg@saltustech.com>
2025-12-30 09:14:24 -0600

    add name attribute

---
+++ SharedScriptsApi/Attributes/TypeNameAttribute.cs
@@ -0,0 +1,42 @@
+﻿namespace SharedScriptsApi.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly | AttributeTargets.Interface, AllowMultiple = true, Inherited = false)]
+    public class TypeNameAttribute : Attribute
+    {
+
+        #region fields/properties
+
+        public string TypeName { get; set; }
+        public string CollectionName { get; set; }
+
+        #endregion
+
+        #region .ctors
+
+        public TypeNameAttribute(string typeName, string collectionName)
+        {
+            TypeName = typeName;
+            CollectionName = collectionName;
+        }
+
+        public TypeNameAttribute(string typeName)
+        {
+            TypeName = typeName;
+            CollectionName = $"{typeName}[]";
+        }
+
+        #endregion
+
+        public static TAttribute[] GetCustomAttributes<TAttribute>(Type source, bool inherit)
+           where TAttribute : Attribute
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+            return source.GetCustomAttributes(typeof(TAttribute), inherit).Cast<TAttribute>().ToArray();
+        }
+    }
+
+
+}
