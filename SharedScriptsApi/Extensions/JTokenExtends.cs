268128a6dab9ddaa685ec430bb8c3baf18797c84
Jason Oehlberg <JOehlberg@saltustech.com>
2025-12-30 09:18:08 -0600

    extensions --working

---
+++ SharedScriptsApi/Extensions/JTokenExtends.cs
@@ -0,0 +1,250 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+namespace SharedScriptsApi.Extensions
+{
+    public static class JTokenExtends
+    {
+        private static string AssemblyFormat => "Saltus.digiTICKET.Data{0}";
+        private static string DefaultVersion => "0110000000";
+        private static string DefaultModelAssemblyName => ModelAssemblyFormatter(DefaultVersion);
+        public static string ModelAssemblyFormatter(string version) => string.Format(AssemblyFormat, version);
+        public static Assembly DefaultModelAssembly => Assembly.Load(DefaultModelAssemblyName);
+        public static Assembly GetVersionedModelAssembly(string version) => Assembly.Load(ModelAssemblyFormatter(version));
+
+        public static bool TryGetValue<T>(this JObject source, string propertyName, StringComparison comparison, out T result)
+        {
+            result = default;
+            return source.TryGetValue(propertyName, comparison, out JToken token) &&
+                token.TryConvertTo(out result);
+        }
+
+        public static bool TryGetValue<T>(this JObject source, string propertyName, StringComparison comparison, string implAssemblyName, out T result)
+        {
+            result = default;
+            return source.TryGetValue(propertyName, comparison, out JToken token) &&
+                token.TryConvertTo(implAssemblyName, out result);
+        }
+
+        public static bool TryGetValue<T>(this JObject source, string propertyName, StringComparison comparison, Assembly implAssembly, out T result)
+        {
+            result = default;
+            return source.TryGetValue(propertyName, comparison, out JToken token) &&
+                token.TryConvertTo(serializer: null, implAssembly, out result);
+        }
+
+        public static bool TryGetValue<T>(this JToken source, string propertyName, StringComparison comparison, out T result)
+        {
+            result = default;
+            return source is JObject jobject && TryGetValue(jobject, propertyName, comparison, out result);
+        }
+
+        /// <summary>
+        /// Checks if a conversion from the supplied <see cref="JToken"/> to a <typeparamref name="T"/> can be made.
+        /// </summary>
+        /// <typeparam name="T">The type to convert to.</typeparam>
+        /// <param name="source">The <see cref="JToken"/>.</param>
+        public static bool CanConvertTo<T>(this JToken source)
+        {
+            return source.TryConvertTo<T>(out _);
+        }
+        /// <summary>
+        /// Checks if a conversion from the supplied <see cref="JToken"/> to a <typeparamref name="TType"/> can be made.
+        /// </summary>
+        /// <typeparam name="T">The type to convert to.</typeparam>
+        /// <param name="source">The <see cref="JToken"/>.</param>
+        /// <param name="result">The result.</param>
+        /// <returns>true is the conversion is successful</returns>
+        public static bool TryConvertTo<T>(this JToken source, out T result)
+        {
+            return TryConvertTo(source, serializer: null, null, out result);
+        }
+
+        public static bool TryConvertTo<T>(this JToken source, string implAssemblyName, out T result)
+        {
+            return TryConvertTo(source, serializer: null, Assembly.Load(implAssemblyName), out result);
+        }
+
+
+        public static bool TryConvertTo<T>(this JToken source, JsonSerializerSettings settings, Assembly assembly, out T result)
+        {
+            var serializer = JsonSerializer.Create(settings);
+            return TryConvertTo(source, serializer, assembly, out result);
+        }
+
+        /// <summary>
+        /// Checks if a conversion from the supplied <see cref="JToken"/> to a <typeparamref name="TType"/> can be made.
+        /// </summary>
+        /// <typeparam name="T">The type to convert to.</typeparam>
+        /// <param name="source">The <see cref="JToken"/>.</param>
+        /// <param name="result">The result.</param>
+        /// <returns>true is the conversion is successful</returns>
+        public static bool TryConvertTo<T>(this JToken source, JsonSerializer serializer, Assembly assembly, out T result)
+        {
+            bool retVal = false;
+            result = default;
+            Type typeT = typeof(T);
+
+            // if the result is an interface
+            if (typeT.IsInterface)
+            {
+                // if the assembly/assembly name has been provided
+                // this uses some fuzzy logic to get the type that implements the interface
+                // this assumes that propertyName supplied in TryGetValue is the name of the type that implements the interface
+                // [WebTeam] if there is a use case not using the above assumption,
+                //          an overload of the TryGetValue method can be created to include the type/type name of the implementing class
+                //          and more processing will need to be done to get the type that implements the interface
+
+                if (assembly != null)
+                {
+                    object impl = null;
+                    Type implType = null;
+
+                    try
+                    {
+                        // get the first type that implements the interface from the assembly
+                        var implTypes = assembly
+                            .GetTypes()
+                            .Where(p => typeT
+                                        .IsAssignableFrom(p) &&
+                                        !p.IsInterface).ToList();
+                        // if there is more than one type that inmpements the interface
+                        if (implTypes.Count() > 1)
+                        {
+                            // get the type by the name of the path of the JToken
+                            implType = implTypes
+                                .Where(p => p.Name.Equals(source.Path, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+                        }
+                        else
+                        {
+                            // else we get first type that implements the interface
+                            implType = implTypes.FirstOrDefault();
+
+                        }
+
+                        if (implType != null)
+                        {
+                            impl = source.ToObject(implType, serializer ?? JsonSerializer.CreateDefault());
+                            result = (T)impl;
+                            retVal = true;
+                        }
+                        else
+                        {
+                            result = default;
+                            retVal = false;
+                        }
+                    }
+                    catch
+                    {
+                        retVal = false;
+                        result = default;
+                    }
+                }
+                else
+                {
+                    object impl = null;
+                    Type implType = null;
+                    try
+                    {
+                        // limit assemblies to the Saltus namespace
+                        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                            .Where(x => x.GetName().Name.StartsWith("Saltus"));
+                        // Get all types that implement the interface
+                        var implTypes = assemblies
+                            .SelectMany(s => s.GetTypes())
+                            .Where(p => typeT
+                                        .IsAssignableFrom(p) &&
+                                        !p.IsInterface).ToList();
+
+                        // if there is more than one type
+                        if (implTypes.Count() > 1)
+                        {
+                            // Get the type by the name of the path of the JToken
+                            var implTypesFromName = implTypes
+                                .Where(p => p.Name.Equals(source.Path, StringComparison.OrdinalIgnoreCase)).ToList();
+
+                            // if there is more than one type with the same name    
+                            if (implTypesFromName.Count() > 1)
+                            {
+                                // use the default Model assembly
+                                var asssembly = Assembly.Load(DefaultModelAssemblyName);
+                                // we can assume that only one type with the path name will be in the default model assembly
+                                // so get the first one from the assembly
+                                implType = implTypesFromName.Where(x => x.Assembly == asssembly).FirstOrDefault();
+                            }
+                            else
+                            {
+                                // get the first type that implements the interface
+                                implType = implTypes.FirstOrDefault();
+                            }
+                        }
+                        else
+                        {
+                            // get the first type that implements the interface
+                            implType = implTypes.FirstOrDefault();
+                        }
+
+                        if (implType != null)
+                        {
+                            impl = source.ToObject(implType, serializer ?? JsonSerializer.CreateDefault());
+                            result = (T)impl;
+                            retVal = true;
+                        }
+                        else
+                        {
+                            result = default;
+                            retVal = false;
+                        }
+                    }
+                    catch
+                    {
+                        result = default;
+                        retVal = false;
+                    }
+                }
+            }
+            else
+            {
+                JToken temp = source;
+                if (temp is JProperty jProperty)
+                {
+                    temp = jProperty.Value;
+                }
+                object sourceValue = temp is JValue jvalue
+                    ? jvalue.Value
+                    : source;
+
+                if (sourceValue == null)
+                {
+                    result = default;
+                    retVal = typeT.IsClass || typeT.GetGenericTypeDefinition() == typeof(Nullable<>);
+                }
+                else if (sourceValue.TryCast(out T cast))
+                {
+                    result = cast;
+                    retVal = true;
+                }
+                else
+                {
+                    try
+                    {
+                        result = source.ToObject<T>(serializer ?? JsonSerializer.CreateDefault());
+                        retVal = !Equals(result, default);
+                    }
+                    catch
+                    {
+                        result = default;
+                        retVal = false;
+                    }
+                }
+            }
+
+
+            return retVal;
+        }
+
+
+
+    }
+}
--- SharedScriptsApi/Extensions/ObjectExtensions.cs
+++ SharedScriptsApi/Extensions/ObjectExtensions.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Linq;
+using SharedScriptsApi.Extensions;
+using SharedScriptsApi.Helpers;
 using System.Reflection;
-using AutoMapper.Internal;
-using Newtonsoft.Json.Linq;
-using Saltus.digiTICKET.Utilities.Extensions;
-using Saltus.digiTICKET.Utilities.Serialization.JSON;
 
 public static class ObjectExtensions
 {
@@ -180,54 +176,54 @@ public static class ObjectExtensions
         return (type.IsValueType & type.IsPrimitive);
     }
 
-    public static Object Copy(this Object originalObject)
-    {
-        return InternalCopy(originalObject, new Dictionary<Object, Object>(new ReferenceEqualityComparer()));
-    }
-    private static Object InternalCopy(Object originalObject, IDictionary<Object, Object> visited)
-    {
-        if (originalObject == null) return null;
-        var typeToReflect = originalObject.GetType();
-        if (IsPrimitive(typeToReflect)) return originalObject;
-        if (visited.ContainsKey(originalObject)) return visited[originalObject];
-        if (typeof(Delegate).IsAssignableFrom(typeToReflect)) return null;
-        var cloneObject = CloneMethod.Invoke(originalObject, null);
-        if (typeToReflect.IsArray)
-        {
-            var arrayType = typeToReflect.GetElementType();
-            if (IsPrimitive(arrayType) == false)
-            {
-                Array clonedArray = (Array)cloneObject;
-                clonedArray.ForEach((array, indices) => array.SetValue(InternalCopy(clonedArray.GetValue(indices), visited), indices));
-            }
+    //public static Object Copy(this Object originalObject)
+    //{
+    //    return InternalCopy(originalObject, new Dictionary<Object, Object>(new ReferenceEqualityComparer()));
+    //}
+    //private static Object InternalCopy(Object originalObject, IDictionary<Object, Object> visited)
+    //{
+    //    if (originalObject == null) return null;
+    //    var typeToReflect = originalObject.GetType();
+    //    if (IsPrimitive(typeToReflect)) return originalObject;
+    //    if (visited.ContainsKey(originalObject)) return visited[originalObject];
+    //    if (typeof(Delegate).IsAssignableFrom(typeToReflect)) return null;
+    //    var cloneObject = CloneMethod.Invoke(originalObject, null);
+    //    if (typeToReflect.IsArray)
+    //    {
+    //        var arrayType = typeToReflect.GetElementType();
+    //        if (IsPrimitive(arrayType) == false)
+    //        {
+    //            Array clonedArray = (Array)cloneObject;
+    //            clonedArray.ForEach((array, indices) => array.SetValue(InternalCopy(clonedArray.GetValue(indices), visited), indices));
+    //        }
 
-        }
-        visited.Add(originalObject, cloneObject);
-        CopyFields(originalObject, visited, cloneObject, typeToReflect);
-        RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect);
-        return cloneObject;
-    }
+    //    }
+    //    visited.Add(originalObject, cloneObject);
+    //    CopyFields(originalObject, visited, cloneObject, typeToReflect);
+    //    RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect);
+    //    return cloneObject;
+    //}
 
-    private static void RecursiveCopyBaseTypePrivateFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect)
-    {
-        if (typeToReflect.BaseType != null)
-        {
-            RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect.BaseType);
-            CopyFields(originalObject, visited, cloneObject, typeToReflect.BaseType, BindingFlags.Instance | BindingFlags.NonPublic, info => info.IsPrivate);
-        }
-    }
+    //private static void RecursiveCopyBaseTypePrivateFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect)
+    //{
+    //    if (typeToReflect.BaseType != null)
+    //    {
+    //        RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect.BaseType);
+    //        CopyFields(originalObject, visited, cloneObject, typeToReflect.BaseType, BindingFlags.Instance | BindingFlags.NonPublic, info => info.IsPrivate);
+    //    }
+    //}
 
-    private static void CopyFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, Func<FieldInfo, bool> filter = null)
-    {
-        foreach (FieldInfo fieldInfo in typeToReflect.GetFields(bindingFlags))
-        {
-            if (filter != null && filter(fieldInfo) == false) continue;
-            if (IsPrimitive(fieldInfo.FieldType)) continue;
-            var originalFieldValue = fieldInfo.GetValue(originalObject);
-            var clonedFieldValue = InternalCopy(originalFieldValue, visited);
-            fieldInfo.SetValue(cloneObject, clonedFieldValue);
-        }
-    }
+    //private static void CopyFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, Func<FieldInfo, bool> filter = null)
+    //{
+    //    foreach (FieldInfo fieldInfo in typeToReflect.GetFields(bindingFlags))
+    //    {
+    //        if (filter != null && filter(fieldInfo) == false) continue;
+    //        if (IsPrimitive(fieldInfo.FieldType)) continue;
+    //        var originalFieldValue = fieldInfo.GetValue(originalObject);
+    //        var clonedFieldValue = InternalCopy(originalFieldValue, visited);
+    //        fieldInfo.SetValue(cloneObject, clonedFieldValue);
+    //    }
+    //}
     //https://github.com/Burtsev-Alexey/net-object-deep-copy
     public static T Copy<T>(this T original)
     {
@@ -262,91 +258,91 @@ public static class ObjectExtensions
 
     public static dynamic GetPropertyValue<T>(this T instance, string name) => instance.GetProperty(name);
 
-    public static void SetDifferences<T>(this T instance, JObject data, string[] excludedProperties, bool includeCollections)
-    {
-        SetDifferencesForUpdate(instance, data, excludedProperties, includeCollections, null, out Dictionary<Type, List<JToken>> values);
-    }
-    /// <summary>
-    /// Sets the property value differences from a JObject to an IAuditable data object
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="instance">T</param>
-    /// <param name="data">JObject</param>
-    public static void SetDifferencesForUpdate<T>(this T instance, JObject data, string[] excludedProperties, bool includeCollections, Dictionary<Type, List<JToken>> seed, out Dictionary<Type, List<JToken>> additionalObjects)
-    {
-        additionalObjects = seed;
+    //public static void SetDifferences<T>(this T instance, JObject data, string[] excludedProperties, bool includeCollections)
+    //{
+    //    SetDifferencesForUpdate(instance, data, excludedProperties, includeCollections, null, out Dictionary<Type, List<JToken>> values);
+    //}
+    ///// <summary>
+    ///// Sets the property value differences from a JObject to an IAuditable data object
+    ///// </summary>
+    ///// <typeparam name="T"></typeparam>
+    ///// <param name="instance">T</param>
+    ///// <param name="data">JObject</param>
+    //public static void SetDifferencesForUpdate<T>(this T instance, JObject data, string[] excludedProperties, bool includeCollections, Dictionary<Type, List<JToken>> seed, out Dictionary<Type, List<JToken>> additionalObjects)
+    //{
+    //    additionalObjects = seed;
 
-        var properties = instance.GetPropertyInfos().Where(x =>
-        !excludedProperties.Any(y => y.Equals(x.Name, StringComparison.OrdinalIgnoreCase)) &&
-        x.SetMethod != null)
-        .ToList();
+    //    var properties = instance.GetPropertyInfos().Where(x =>
+    //    !excludedProperties.Any(y => y.Equals(x.Name, StringComparison.OrdinalIgnoreCase)) &&
+    //    x.SetMethod != null)
+    //    .ToList();
 
-        foreach (var prop in properties)
-        {
-            if (prop.PropertyType.IsDtModel())
-            {
-                dynamic value = properties[properties.IndexOf(prop)].GetValue(instance);
+    //    foreach (var prop in properties)
+    //    {
+    //        if (prop.PropertyType.IsDtModel())
+    //        {
+    //            dynamic value = properties[properties.IndexOf(prop)].GetValue(instance);
 
-                if (value != null)
-                {
-                    var subObject = data.GetValue(prop.Name, StringComparison.OrdinalIgnoreCase);
-                    if (subObject != null)
-                    {
-                        SetDifferencesForUpdate(value, (JObject)subObject, excludedProperties, includeCollections, seed, out additionalObjects);
-                    }
-                }
-            }
-            else if (prop.PropertyType.IsDatabaseType())
-            {
-                dynamic value = properties[properties.IndexOf(prop)].GetValue(instance);
+    //            if (value != null)
+    //            {
+    //                var subObject = data.GetValue(prop.Name, StringComparison.OrdinalIgnoreCase);
+    //                if (subObject != null)
+    //                {
+    //                    SetDifferencesForUpdate(value, (JObject)subObject, excludedProperties, includeCollections, seed, out additionalObjects);
+    //                }
+    //            }
+    //        }
+    //        else if (prop.PropertyType.IsDatabaseType())
+    //        {
+    //            dynamic value = properties[properties.IndexOf(prop)].GetValue(instance);
 
-                if (value != null)
-                {
-                    SetValue(value, instance, prop, data);
-                }
-            }
-            else if (prop.PropertyType.IsCollection())
-            {
-                if (includeCollections)
-                {
-                    IEnumerable<dynamic> collection = (IEnumerable<dynamic>)properties[properties.IndexOf(prop)].GetValue(instance);
+    //            if (value != null)
+    //            {
+    //                SetValue(value, instance, prop, data);
+    //            }
+    //        }
+    //        else if (prop.PropertyType.IsCollection())
+    //        {
+    //            if (includeCollections)
+    //            {
+    //                IEnumerable<dynamic> collection = (IEnumerable<dynamic>)properties[properties.IndexOf(prop)].GetValue(instance);
 
-                    var items = collection.ToList();
+    //                var items = collection.ToList();
 
-                    if (data.TryGetValue(prop.Name, StringComparison.OrdinalIgnoreCase, out JArray jArray))
-                    {
-                        if (items != null && items.Any())
-                        {
-                            string idName = $"{items.First().GetType().Name}Id";
-                            List<int> existingIds = items.Select(x => x[idName]).Cast<int>().ToList();
+    //                if (data.TryGetValue(prop.Name, StringComparison.OrdinalIgnoreCase, out JArray jArray))
+    //                {
+    //                    if (items != null && items.Any())
+    //                    {
+    //                        string idName = $"{items.First().GetType().Name}Id";
+    //                        List<int> existingIds = items.Select(x => x[idName]).Cast<int>().ToList();
 
-                            foreach (dynamic item in items)
-                            {
-                                dynamic id = GetPropertyValue(item, idName);
+    //                        foreach (dynamic item in items)
+    //                        {
+    //                            dynamic id = GetPropertyValue(item, idName);
 
-                                if (id != null)
-                                {
-                                    var jToken = jArray.FirstOrDefault(x => x[idName] == id);
+    //                            if (id != null)
+    //                            {
+    //                                var jToken = jArray.FirstOrDefault(x => x[idName] == id);
 
-                                    if (jToken != null)
-                                    {
-                                        SetDifferencesForUpdate(item, (JObject)jToken, excludedProperties, true, seed, out additionalObjects);
-                                    }
-                                }
-                            }
+    //                                if (jToken != null)
+    //                                {
+    //                                    SetDifferencesForUpdate(item, (JObject)jToken, excludedProperties, true, seed, out additionalObjects);
+    //                                }
+    //                            }
+    //                        }
 
-                            // this should never be hit be exectued if the additional objects we recieved 
-                            // on the first run for the queried object
-                            if (jArray.Count > items.Count())
-                            {
-                                Type castType = items.First().GetType();
-                                var list = jArray.Where(x => !(x as JObject).ContainsKey(idName)).ToList();
-                                additionalObjects.Add(castType, list);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    //                        // this should never be hit be exectued if the additional objects we recieved 
+    //                        // on the first run for the queried object
+    //                        if (jArray.Count > items.Count())
+    //                        {
+    //                            Type castType = items.First().GetType();
+    //                            var list = jArray.Where(x => !(x as JObject).ContainsKey(idName)).ToList();
+    //                            additionalObjects.Add(castType, list);
+    //                        }
+    //                    }
+    //                }
+    //            }
+    //        }
+       // }
+    //}
 }
